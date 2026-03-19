996da49443f57238f136df374f30d3f53f597586
Maor Sharon <maor@zappy5.com>
2026-03-19 20:27:51 +0200

    Background sync: Progressive GitHub Sync

--- script.js
+++ script.js
@@ -1286,6 +1286,10 @@ window.onload = function() {
     
 /* ZAPPY_INTEGRATION_END unknown:setup_contact_form_emails */
 ;
+
+;
+
+;
 /* ==ZAPPY E-COMMERCE JS START== */
 // E-commerce functionality
 (function() {
